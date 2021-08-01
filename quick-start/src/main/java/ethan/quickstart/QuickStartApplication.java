ded875a18cce074105db88bf890ee1915e3bf306
--replace-all <k435467@gmail.com>
2021-08-01 11:43:35 +0800

    start a new spring boot project

--- quick-start/src/main/java/ethan/quickstart/QuickStartApplication.java
+++ quick-start/src/main/java/ethan/quickstart/QuickStartApplication.java
@@ -2,12 +2,21 @@ package ethan.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController  // tells Spring that this code describes an endpoint that should be made available over the web.
 public class QuickStartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickStartApplication.class, args);
 	}
 
+	@GetMapping("/hello")  // answer requests that send to url/hello
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		// expect a name value in the request, but if it’s not there, it will use the word “World” by default.
+		return String.format("Hello %s!", name);
+	}
 }
